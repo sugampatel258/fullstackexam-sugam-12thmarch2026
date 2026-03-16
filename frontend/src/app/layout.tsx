2dc3b5170f4f191497d1ed38510396c3e2ed8f86
sugampatel258 <sugampatel258@gmail.com>
2026-03-16 20:52:28 +0530

    navbar issue

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,8 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
+  <Navbar />;
 }
